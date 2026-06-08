ab9fe266bac08d666242ebe2a0e91e2b1a2a2bf1
shyamdigitalit <donotreply.digital@shyamgroup.com>
2026-06-08 17:34:39 +0530

    serverless setup done

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ import mongoConn from "./db/dbcon.js";
 
 export default async function handler(req, res) {
     await mongoConn();
+    req.url = req.url.replace(/^\/api/, "");
+    console.log(req.url);
     return app(req, res);
 }
