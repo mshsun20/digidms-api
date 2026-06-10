f8af5fb0acade3a9a9ff9196d4912811cac5b453
shyamdigitalit <donotreply.digital@shyamgroup.com>
2026-06-10 14:34:07 +0530

    serverless conversion started

--- index.js
+++ index.js
@@ -4,22 +4,7 @@ import mongoConn from "./db/dbcon.js";
 let isConnected = false;
 
 export default async function handler(req, res) {
-    try {
-
-        if (!isConnected) {
-            await mongoConn();
-            isConnected = true;
-        }
-
-        return app(req, res);
-
-    } catch (err) {
-
-        console.error(err);
-
-        return res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
+    await mongoConn();
+    req.url = req.url.replace(/^\/api/, "");
+    console.log(req.url);
 }
